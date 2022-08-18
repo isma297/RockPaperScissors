a120b7fad6988f22fcd7ddf9502071ad2dbe0176
isma297 <ismael.vega297@gmail.com>
2022-08-18 19:53:02 -0300

    add buttons to play R,P,S by clicking

--- script.js
+++ script.js
@@ -1,55 +1,60 @@
-// Computer play
-let cpu;
+// Computer play--- choose r,p,s randomly
+let roll=0;
+let cpu='';
 function computerPlay() {
-    cpu=Math.floor(Math.random()*100);
-    if(cpu<33){
-        cpu="rock";
-         }
-    else if(cpu <66){cpu="paper";
-        }
-    else {cpu="scissors";
-          }   
-            return cpu;     
-            }
-//Player Play
-let player;
-function playerPlay(){
-    player=(prompt("Input your move",""))
-    player=player.toLowerCase();
-    if (player!=="rock"&&player!=="paper" && player!=="scissors"){
-    //console.log("Only availables moves are: rock, paper o scissor");
-    alert("Only availables moves are: rock, paper o scissor");
-    playerPlay();
-    }
-    return player;   
-    }
+    roll=Math.floor(Math.random()*100);
+    if(roll<33){ cpu="rock";}
+    else if(roll <66){cpu="paper";}
+    else {cpu="scissors";}   
+        return cpu;}
+//Player Play ---- function to input with keybord and play trough console
+// REPLACED WITH BUTTONS.
+// let player;
+// function playerPlay(){
+//     player=(prompt("Input your move",""))
+//     player=player.toLowerCase();
+//     if (player!=="rock"&&player!=="paper" && player!=="scissors"){
+//     //console.log("Only availables moves are: rock, paper o scissor");
+//     alert("Only availables moves are: rock, paper o scissor");
+//     playerPlay();
+//     }
+//     return player;   
+//     }
 //Play the Game!
+const container=document.querySelector('#container');
+const p= document.createElement('p');
 let winner;
 let scorePlayer=0;
 let scoreComputer=0;
-function play(){
+// BUTTONS to play
+const btnRock = document.querySelector('#rock');
+btnRock.addEventListener('click',()=>play('rock'))
+const btnPaper = document.querySelector('#paper');
+btnPaper.addEventListener('click',()=>play('paper'))  
+const btnScissors = document.querySelector('#scissors');
+btnScissors.addEventListener('click',()=>play('scissors'))  
+
+function play(hand){
    // for(let i=0; i<5; i++){
-        computerPlay();
-        playerPlay();
-        // console.log(cpu);
-        // console.log(player);
-
-        if(cpu===player){
-        // console.log("You chose "+ player+ " and the computer chose "+ cpu +" It is a Tie!" );
+       computerPlay();
+        if(cpu===hand){
+        p.textContent='this is a tie';
+        container.appendChild(p);
         }
-        else if (cpu=="rock"&&player=="scissors" ||  cpu=="paper"&&player=="rock"|| cpu=="scissors"&&player=="paper"){
-        scoreComputer++;
-        // console.log("You chose "+ player+ " and the computer chose " + cpu +" The computer obtained 1 point!");
+        else if (cpu=="rock"&&hand=="scissors" ||  cpu=="paper"&&hand=="rock"|| cpu=="scissors"&&hand=="paper"){
+        //scoreComputer++;
+        p.textContent='Cpu wins';
+        container.appendChild(p);
         }
-        else{ scorePlayer++;
-            //  console.log("You chose "+ player+ " and the computer chose "+ cpu +" You obtained 1 point!")};
+        else{ //scorePlayer++;
+            p.textContent='you win';
+            container.appendChild(p);
             }
-            // console.log("Player score: "+scorePlayer+" ----- Computer score: " +scoreComputer);
-            scoreComputer == scorePlayer ? winner="tie" :(scoreComputer > scorePlayer ? winner="The computer won": winner="the player won");
-            // console.log(winner.toUpperCase());
-            return winner;
+           
+           // scoreComputer == scorePlayer ? winner="tie" :(scoreComputer > scorePlayer ? winner="The computer won": winner="the player won");
+        
+           // return winner;
         }
 
-        
 
 //play();
