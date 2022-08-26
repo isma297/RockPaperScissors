bd3511cd0e82c27c8c6e8c34ccbed283745e0685
isma297 <ismael.vega297@gmail.com>
2022-08-26 11:20:09 -0300

    modified ui v1

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function computerPlay() {
     else if(roll <66){cpu="paper";}
     else {cpu="scissors";}   
         return cpu;}
-//Player Play ---- function to input with keybord and play trough console
+//Player Play ---- function: input with keybord and play trough console
 // REPLACED WITH BUTTONS.
 // let player;
 // function playerPlay(){
@@ -21,40 +21,56 @@ function computerPlay() {
 //     return player;   
 //     }
 //Play the Game!
-const container=document.querySelector('#container');
-const p= document.createElement('p');
-let winner;
-let scorePlayer=0;
-let scoreComputer=0;
+const container2=document.querySelector('#container2');
+const handPlayed= document.createElement('p');
+const roundResult=document.createElement('p');
+const result=document.createElement('p');
+const divWin=document.createElement('p');
+divWin.setAttribute('class','winner');
+let round=0;
+let playerScore=0;
+let cpuScore=0;
 // BUTTONS to play
-const btnRock = document.querySelector('#rock');
-btnRock.addEventListener('click',()=>play('rock'))
-const btnPaper = document.querySelector('#paper');
-btnPaper.addEventListener('click',()=>play('paper'))  
-const btnScissors = document.querySelector('#scissors');
-btnScissors.addEventListener('click',()=>play('scissors'))  
+// const btnRock = document.querySelector('#rock');
+// const btnPaper = document.querySelector('#paper');
+// const btnScissors = document.querySelector('#scissors');
+// btnRock.addEventListener('click',()=>play('rock'));
+// btnPaper.addEventListener('click',()=>play('paper'));  
+// btnScissors.addEventListener('click',()=>play('scissors'));
+
+const buttons=document.querySelectorAll('button');
+buttons.forEach((button)=>{button.addEventListener('click',()=>{play(button.id)})
+});
 
 function play(hand){
-   // for(let i=0; i<5; i++){
-       computerPlay();
-        if(cpu===hand){
-        p.textContent='this is a tie';
-        container.appendChild(p);
+  if(round<5){
+  round++; 
+  computerPlay();
+  if(cpu===hand){
+    handPlayed.textContent='Player:   -'+hand.toUpperCase()+'-    vs    -'+cpu.toUpperCase()+'-   :Cpu';
+    result.textContent='Round '+round+': Tie, no points :('
+    }
+  else if (cpu=="rock"&&hand=="scissors" ||  cpu=="paper"&&hand=="rock"|| cpu=="scissors"&&hand=="paper"){
+    cpuScore++;
+    handPlayed.textContent='Player:   -'+ hand.toUpperCase() +'-    vs    -'+ cpu.toUpperCase()+'-   :Cpu';
+    result.textContent='Round '+round+': +1 to Cpu'
+    }
+  else{playerScore++;
+        handPlayed.textContent='Player:   -'+ hand.toUpperCase() +'-    vs    -'+ cpu.toUpperCase()+'-   :Cpu';
+        result.textContent='Round '+round+': +1 to Player'
         }
-        else if (cpu=="rock"&&hand=="scissors" ||  cpu=="paper"&&hand=="rock"|| cpu=="scissors"&&hand=="paper"){
-        //scoreComputer++;
-        p.textContent='Cpu wins';
-        container.appendChild(p);
-        }
-        else{ //scorePlayer++;
-            p.textContent='you win';
-            container.appendChild(p);
-            }
-           
-           // scoreComputer == scorePlayer ? winner="tie" :(scoreComputer > scorePlayer ? winner="The computer won": winner="the player won");
-        
-           // return winner;
-        }
+  roundResult.textContent='Player '+playerScore+'   :  '+ cpuScore+ ' Cpu';
+  container2.appendChild(handPlayed);
+  container2.appendChild(result);
+  container2.appendChild(roundResult);
+  }
+if(round==5) {playerScore==cpuScore? winner='It is a TIE':(playerScore>cpuScore?winner='YOU WIN!':winner='CPU WINS!')
+divWin.textContent=winner;
+container2.appendChild(divWin)
+}
+//    
+
+}
 
 
 //play();
